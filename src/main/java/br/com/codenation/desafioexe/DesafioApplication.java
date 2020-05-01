b316b1d16e0cbca53c0890251a6636905eacfa4a
Montival Junior <montival_junior@yahoo.com.br>
2020-05-01 12:59:13 -0300

    Envio de resolução primeiro desafio aceleradev Java

--- src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
+++ src/main/java/br/com/codenation/desafioexe/DesafioApplication.java
@@ -4,21 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static java.util.stream.Stream.*;
+
 public class DesafioApplication {
 
     public static List<Integer> fibonacci() {
         List<Integer> arrayFibonacci = new ArrayList<>();
-        Stream.iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
+        iterate(new int[]{0, 1}, n -> new int[]{n[1], n[0] + n[1]})
                 .limit(15)
                 .map(n -> n[0])
-                .forEach(x -> arrayFibonacci.add(x));
+                .forEach(arrayFibonacci::add);
 
         return arrayFibonacci;
+
     }
 
     public static Boolean isFibonacci(Integer a) {
 
         return fibonacci().contains(a);
+
     }
 }
 
